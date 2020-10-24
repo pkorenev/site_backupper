4dc0a6d02eb90e40634c1db15f0ec58d907ed0bd
pkorenev <p.korenev@voroninstudio.eu>
2020-10-24 22:32:08 +0300

    initial commit

---
+++ lib/site_backupper.rb
@@ -0,0 +1,6 @@
+require "site_backupper/version"
+
+module SiteBackupper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/site_backupper/version.rb
@@ -0,0 +1,3 @@
+module SiteBackupper
+  VERSION = "0.1.0"
+end
